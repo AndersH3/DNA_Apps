aaade539260dd397de28e1556978ee7f6ced4951
Anders Hellström <anders@hellstrom.biz>
2018-04-01 15:59:07 +0200

    Add files via upload

--- merge_ydata.py
+++ merge_ydata.py
@@ -1,42 +1,48 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
 # Use only with files in ftdna format
-#you can reformat your data with [https://ytree.morleydna.com/extractFromAutosomal]
-#just copy and paste the data from the last step into a file.
-#But do not hand edit the data.
-#If you want to mannualy add some snps add them into a separate file eg extra.txt.
-# Usage python3.6 merge_ydata.py file1.txt file2.txt ... [extra.txt]
-# TODO Statistics file created.
+# you can reformat your data with [https://ytree.morleydna.com/extractFromAutosomal]
+# just copy and paste the data from the last step into a file.
+# But do not hand edit the data.
+# If you want to manually add some snps add them into a separate file eg extra.txt.
+# Usage python3.7 merge_ydata.py file1.txt file2.txt ... [extra.txt]
+# T O D O Statistics file created.
+
+
 import sys
+from random import *
+from re import *
 
 
 def process(input_data):
-    results = []
-    input_data = input_data.strip().split(",")
-    for cell in input_data:
-        inp = cell.strip().replace(" ", "").replace("\n", "")
+    results = set([])
+    white_space = compile(r"\s")
+    for inp in input_data:
+        inp = white_space.sub("", inp)
         if inp != '':
-            results.append(inp)
-    results = sorted(set(results))
-    return results
+            results.add(inp)
+    return sorted(results)
 
 
-# if __name__ == '__main__':
-#    pth = ""
-#    file_list = os.listdir("./" + pth)
-
-if len(sys.argv) > 1:
-    file_location = sys.argv[1].strip()
-    with open(file_location, 'r') as input_data_file:
-        the_input_data = input_data_file.read()
-else:
-    print('This test requires some input files.')
-    exit(1)
-
-the_results = process(the_input_data)
-
-new_file_location = file_location + ".out.txt"
-f = open(new_file_location, "w")
-f.write(",".join(the_results))
-exit(0)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        data = []
+        first = 0
+        pattern = compile(r"merge_ydata")
+        for file_location in sys.argv:
+            file_location = file_location.strip()
+            if pattern.match(file_location):
+                first += 1
+            elif first > 0:
+                with open(file_location, 'r') as input_data_file:
+                    the_input_data = input_data_file.read()
+                    data.extend(process(the_input_data.split(",")))
+        the_results = sorted(set(data))
+        id = round(random() * 10000)
+        f = open(str(id) + ".out.txt", "w")
+        f.write(",".join(the_results))
+        exit(0)
+    else:
+        print('This test requires some input files.')
+        exit(1)
