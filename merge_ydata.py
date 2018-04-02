50c1863ee6fe1660b616d7590e5a92869ac1b118
Hellstrom-179 <37925579+Hellstrom-179@users.noreply.github.com>
2018-04-02 04:04:59 +0200

    Add files via upload

--- merge_ydata.py
+++ merge_ydata.py
@@ -17,17 +17,27 @@ from re import *
 
 def process(input_data):
     results = set([])
+    positive = set([])
+    negative = set([])
     white_space = compile(r"\s")
     for inp in input_data:
         inp = white_space.sub("", inp)
         if inp != '':
             results.add(inp)
-    return sorted(results)
+            if inp.endswith('+'):
+                positive.add(inp)
+            elif inp.endswith('-'):
+                negative.add(inp)
+            else:
+                print(inp)
+    return [sorted(results), sorted(positive), sorted(negative)]
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         data = []
+        data_positive = []
+        data_negative = []
         first = 0
         pattern = compile(r"merge_ydata")
         for file_location in sys.argv:
@@ -37,12 +47,23 @@ if __name__ == '__main__':
             elif first > 0:
                 with open(file_location, 'r') as input_data_file:
                     the_input_data = input_data_file.read()
-                    data.extend(process(the_input_data.split(",")))
-        the_results = sorted(set(data))
-        id = round(random() * 10000)
-        f = open(str(id) + ".out.txt", "w")
-        f.write(",".join(the_results))
+                    res = process(the_input_data.split(","))
+                    data.extend(res[0])
+                    data_positive.extend(res[1])
+                    data_negative.extend(res[2])
+        nr = round(random() * 10000)
+        f = open(str(nr) + ".out.txt", "w")
+        f.write(",".join(sorted(set(data))))
+        f = open(str(nr) + ".positive.out.txt", "w")
+        f.write(",".join(data_positive))
+        f = open(str(nr) + ".negative.out.txt", "w")
+        f.write(",".join(data_negative))
+        report = "snps: " + str(len(data)) + "   " \
+                 + "snps(positive): " + str(len(data_positive)) + "   " \
+                 + "snps(negative): " + str(len(data_negative)) + "   "
+        f = open(str(nr) + ".report.out.txt", "w")
+        f.write(report)
         exit(0)
     else:
-        print('This test requires some input files.')
+        print('This requires some input files.')
         exit(1)
